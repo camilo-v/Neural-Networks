670eac19416ed05e62e13e08b4c021359dca9adb
Camilo <camilo@castflyer.com>
2017-05-30 17:08:20 -0400

    Initial commit and implementation of the Hebbian learning algorithm.

---
+++ hebbian/hebbian.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3
+
+# ---------------------------------------------------------------------------------------------------------------------
+#
+#                                        Bioinformatics Research Group
+# 									        http://biorg.cis.fiu.edu/
+#                             			Florida International University
+#
+#   This software is a "Camilo Valdes Work" under the terms of the United States Copyright Act.
+#   Please cite the author(s) in any work or product based on this material.
+#
+#   OBJECTIVE:
+#   The purpose of this program is to implement the Hebbian classifier.
+#
+#   NOTES:
+#   Please see the dependencies section below for the required libraries (if any).
+#
+#   DEPENDENCIES:
+#
+#       • Numpy
+#
+#   The above libraries & modules are required. You can check the modules currently installed in your
+#   system by running: python -c "help('modules')"
+#
+#   USAGE:
+#   Run the program with the "--help" flag to see usage instructions.
+#
+#   AUTHOR:	Camilo Valdes (cvalde03@fiu.edu)
+#           Bioinformatics Research Group,
+#           School of Computing and Information Sciences,
+#           Florida International University (FIU)
+#
+#
+# ---------------------------------------------------------------------------------------------------------------------
