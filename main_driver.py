88eee158037ae728494a52392cdbaa240582fcca
Camilo <camilo@castflyer.com>
2017-05-30 17:18:22 -0400

    Refactoring import statements.

--- main_driver.py
+++ main_driver.py
@@ -38,12 +38,18 @@ import os
 import sys
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 import time
-import perceptron
 
 #   Custom Modules
 sys.path.append(os.path.join(os.path.dirname(__file__), ''))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'perceptron'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'adaline'))
+sys.path.append(os.path.join(os.path.dirname(__file__), 'hebbian'))
 import utility_functions as uf
+import perceptron
+import adaline
+import hebbian
 
 # ------------------------------------------------------ Main ---------------------------------------------------------
 #
@@ -166,3 +172,29 @@ for xi, target in zip(df_testingSet_3_X, df_y):
 
 print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Test Set 3 Error: " +
        str(testingSet_3_error) )
+
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" )
+
+
+# ----------------------------------------------------- Adaline -------------------------------------------------------
+#
+#   Train the Adaline classifier algorithm.
+#
+
+#   Matplotlib objects to hold dual-pane figure
+fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(8, 4))
+
+ada1 = adaline.Adaline(n_iter=10, eta=0.01).fit( df_X, df_y )
+ax[0].plot(range(1, len(ada1.cost_) + 1), np.log10(ada1.cost_), marker='o')
+ax[0].set_xlabel('Epochs')
+ax[0].set_ylabel('log(Sum-Squared-Error)')
+ax[0].set_title('Adaline - Learning rate 0.01')
+
+ada2 = adaline.Adaline(n_iter=10, eta=0.0001).fit( df_X, df_y )
+ax[1].plot(range(1, len(ada2.cost_) + 1), ada2.cost_, marker='o')
+ax[1].set_xlabel('Epochs')
+ax[1].set_ylabel('Sum-Squared-Error')
+ax[1].set_title('Adaline - Learning rate 0.0001')
+
+plt.tight_layout()
+plt.show()
