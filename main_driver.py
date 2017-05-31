e898f84bad8d7c87e78f988bab1774aa5b836d4b
Camilo <camilo@castflyer.com>
2017-05-30 22:17:34 -0400

    Refactoring to add Hebbian learning trials.

--- main_driver.py
+++ main_driver.py
@@ -55,7 +55,8 @@ import hebbian
 #
 #
 print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S',time.localtime()) + " ]" )
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S',time.localtime()) + " ]" + " Perceptron" + "" )
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S',time.localtime()) + " ]" + " Project 1 - Single Layer Networks" + "" )
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S',time.localtime()) + " ]" )
 
 #   Training Data for letter A
 filePathForTraining_A = "/Users/camilo/Documents/Development/GitHub/Neural-Networks/data/letters/data-A-training.txt"
@@ -72,59 +73,19 @@ print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Lo
 #   Load the training data for the letter A, and get back an array with target values (y) and feature matrix (X).
 trainingTargetValues_y, trainingFeatureMatrix_X = uf.readTrainingData(filePathForTraining_A)
 
-# -------------------------------------------- Training Data Inspection -----------------------------------------------
-
-print("Target Values (y): ")
-print(trainingTargetValues_y)
-
-print("Feature Matrix (X): ")
-print(trainingFeatureMatrix_X)
-
-# --------------------------------------------------- Perceptron ------------------------------------------------------
-#
-#   Train the perceptron algorithm.
-#
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" )
 print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Creating Data Structures..." + "" )
 
-#   Create a Pandas Dataframe from the regular array
-#   .T transposes the array, and as_matrix() converts it to a numpy.ndarray
+#   Create a Pandas Dataframe from the regular array. Note tha as_matrix() converts it to a numpy.ndarray
+#   The target values (ground truth)
 df_y = pd.DataFrame(trainingTargetValues_y).as_matrix()
-# print(df_y)
-
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" )
-
 #   Create a Pandas Dataframe from the regular feature matrix
-#   as_matrix() converts it to a numpy.ndarray
 df_X = pd.DataFrame(trainingFeatureMatrix_X).as_matrix()
-# print(df_X)
 
-#
-#   Initialize the Perceptron, and train it with the "fit" method.
-#
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Initializing Perceptron..." + "" )
-ppn = perceptron.Perceptron( eta=0.1, n_iter=10 )
-
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Training Perceptron..." + "" )
-ppn.fit( df_X, df_y )
-
-# -------------------------------------------- Perceptron Training Errors ---------------------------------------------
-#
-#   Plot the misclassification errors versus the number of epochs to see if the Perceptron converges after a given
-#   number of epochs.
-#
-
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Plotting Training Errors..." + "" )
-
-plt.plot(range(1, len(ppn.errors_) + 1), ppn.errors_, marker='o')
-plt.xlabel('Epochs')
-plt.ylabel('Number of Misclassifications')
-plt.tight_layout()
-# plt.show()
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S',time.localtime()) + " ]" )
 
 # ----------------------------------------------- Testing Data Loading ------------------------------------------------
 #
-#   Now that we have a fully trainined Perceptron, we can go ahead and load the testing data to "test" it.
+#   Load the test datasets.
 #
 print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Loading Testing data..." + "" )
 
@@ -138,40 +99,107 @@ df_testingSet_1_X = pd.DataFrame( testingSet1FeatureMatrix_X ).as_matrix()
 df_testingSet_2_X = pd.DataFrame( testingSet2FeatureMatrix_X ).as_matrix()
 df_testingSet_3_X = pd.DataFrame( testingSet3FeatureMatrix_X ).as_matrix()
 
-# ------------------------------------------------ Perceptron Testing -------------------------------------------------
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S',time.localtime()) + " ]" )
+
+# ------------------------------------------------ Hebbian Learning ---------------------------------------------------
 #
-#   Now that we have loaded the testing sets as feature matrices, we will use the Perceptron's class "predict" method
-#   to test the classifier with a given testing set
+#   Initialize the Hebb classifier and train it with the preloaded training data.
+#   Note that n_iter is set to 1, it can be changed but the Hebb implementation will ignore it. It is used here for
+#   the sake of debugging.
 #
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Initializing Hebb..." + "" )
+
+hebb = hebbian.Hebbian( eta=0.1, n_iter=1 )
+hebb.fit( df_X, df_y )
+
 print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Running testing datasets..." + "" )
 
-testingSet_1_error = 0
-testingSet_2_error = 0
-testingSet_3_error = 0
+HebbTestingSet_1_error = 0
+HebbTestingSet_2_error = 0
+HebbTestingSet_3_error = 0
+
+#   Testing Dataset I with Hebb
+for xi, target in zip(df_testingSet_1_X, df_y):
+    testResult = target - hebb.predict( xi )
+    print(testResult)
+    HebbTestingSet_1_error += int( testResult != 0.0 )
+
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Hebb Test Set 1 Error: " +
+       str(HebbTestingSet_1_error) )
+
+#   Testing Dataset II with Hebb
+for xi, target in zip(df_testingSet_2_X, df_y):
+    testResult = target - hebb.predict( xi )
+    print( testResult )
+    HebbTestingSet_2_error += int( testResult != 0.0 )
+
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Hebb Test Set 2 Error: " +
+       str(HebbTestingSet_2_error) )
+
+#   Testing Dataset III with Hebb
+for xi, target in zip(df_testingSet_3_X, df_y):
+    testResult = target - hebb.predict( xi )
+    print( testResult )
+    HebbTestingSet_3_error += int( testResult != 0.0 )
+
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Hebb Test Set 3 Error: " +
+       str(HebbTestingSet_3_error) )
+
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" )
+
+# --------------------------------------------------- Perceptron ------------------------------------------------------
+#
+#   Initialize the Perceptron, and train it with the "fit" method.
+#
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Initializing Perceptron..." + "" )
+ppn = perceptron.Perceptron( eta=0.1, n_iter=10 )
+
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Training Perceptron..." + "" )
+ppn.fit( df_X, df_y )
+
+#   Perceptron Training Errors
+#   Plot the misclassification errors versus the number of epochs to see if the Perceptron converges after a given
+#   number of epochs.
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Plotting Training Errors..." + "" )
+
+plt.plot(range(1, len( ppn.errors_ ) + 1), ppn.errors_, marker='o')
+plt.xlabel('Epochs')
+plt.ylabel('Number of Misclassifications')
+plt.tight_layout()
+# plt.show()
+
+#   Perceptron Testing
+#   Now that we have loaded the testing sets as feature matrices, we will use the Perceptron's class "predict" method
+#   to test the classifier with a given testing set
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Running testing datasets..." + "" )
+
+PerceptronTestingSet_1_error = 0
+PerceptronTestingSet_2_error = 0
+PerceptronTestingSet_3_error = 0
 
 #   Testing Dataset I
 for xi, target in zip(df_testingSet_1_X, df_y):
     testResult = target - ppn.predict( xi )
-    testingSet_1_error += int( testResult != 0.0 )
+    PerceptronTestingSet_1_error += int( testResult != 0.0 )
 
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Test Set 1 Error: " +
-       str(testingSet_1_error) )
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Perceptron Test Set 1 Error: " +
+       str(PerceptronTestingSet_1_error) )
 
 #   Testing Dataset II
 for xi, target in zip(df_testingSet_2_X, df_y):
     testResult = target - ppn.predict( xi )
-    testingSet_2_error += int( testResult != 0.0 )
+    PerceptronTestingSet_2_error += int( testResult != 0.0 )
 
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Test Set 2 Error: " +
-       str(testingSet_2_error) )
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Perceptron Test Set 2 Error: " +
+       str(PerceptronTestingSet_2_error) )
 
 #   Testing Dataset III
 for xi, target in zip(df_testingSet_3_X, df_y):
     testResult = target - ppn.predict( xi )
-    testingSet_3_error += int( testResult != 0.0 )
+    PerceptronTestingSet_3_error += int( testResult != 0.0 )
 
-print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Test Set 3 Error: " +
-       str(testingSet_3_error) )
+print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" + " Perceptron Test Set 3 Error: " +
+       str(PerceptronTestingSet_3_error) )
 
 print( "[ " + time.strftime('%d-%b-%Y %H:%M:%S', time.localtime()) + " ]" )
 
