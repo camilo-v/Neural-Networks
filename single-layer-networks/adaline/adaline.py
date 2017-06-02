5d75fe53b7fc42e9feffd21c1b1a2d644d73b527
Camilo <camilo@castflyer.com>
2017-06-02 17:32:11 -0400

    Moving project 1 files into their own directory, and recommitting moved files.

---
+++ single-layer-networks/adaline/adaline.py
@@ -0,0 +1,133 @@
+#!/usr/local/bin/python3
+
+# ---------------------------------------------------------------------------------------------------------------------
+#
+#                                        Bioinformatics Research Group
+# 									        http://biorg.cis.fiu.edu/
+#                             			Florida International University
+#
+#   This software is a "Camilo Valdes Work" under the terms of the United States Copyright Act.
+#   Please cite the author(s) in any work or product based on this material.
+#
+#   OBJECTIVE:
+#   The purpose of this program is to implement the Adaptive Linear Neuron (ADALINE) classifier.
+#
+#   NOTES:
+#   Please see the dependencies section below for the required libraries (if any).
+#
+#   DEPENDENCIES:
+#
+#       • Pandas
+#       • Numpy
+#
+#   The above libraries & modules are required. You can check the modules currently installed in your
+#   system by running: python -c "help('modules')"
+#
+#   USAGE:
+#   Run the program with the "--help" flag to see usage instructions.
+#
+#   AUTHOR:	Camilo Valdes (cvalde03@fiu.edu)
+#           Bioinformatics Research Group,
+#           School of Computing and Information Sciences,
+#           Florida International University (FIU)
+#
+#
+# ---------------------------------------------------------------------------------------------------------------------
+
+# 	Python Modules
+import numpy as np
+
+class Adaline(object):
+    """
+    Perceptron Classifier.
+    This module implements the Perceptron Learning algorithm.
+
+    Parameters:
+        eta (float): Learning rate (between 0.0 and 1.0)
+        n_iter (int): Number of passes over the entire dataset (epochs).
+
+    Attributes:
+        w_ (1-d array): Weights after fitting.
+        cost_ (list): Sum-of-squares cost function value in each epoch.
+    """
+
+    def __init__(self, eta=0.01, n_iter=50):
+        """
+        Default initilizer.
+        Args:
+            eta: The learning rate.
+            n_iter: number of iterations (epochs).
+        """
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        """
+        Fit the Adaline instance with the training data.
+        Args:
+            X (numpy.ndarray): Feature matrix, where n_samples is the number of samples and n_features is the
+                number of features.
+            y (numpy.ndarray): Target values.
+
+        Returns:
+            self An object type.
+        """
+
+        self.w_ = np.array(np.zeros( 1 + X.shape[1] ))
+        self.w_ = self.w_[:, np.newaxis]    # Adds a new axis -> 2D array. Required to update the weights.
+        self.cost_ = []
+
+        for i in range(self.n_iter):
+
+            output = self.activation( X )
+
+            errors = (y - output)
+
+            #   Calculate the gradient based on the whole training dataset for weights 1 to m
+            #   Note that np.asarray(self.w_[1:]) is required so that Numpy can see the vector of weights
+            #   correctly and it can perform the dot product.
+            self.w_[1:] = np.add( np.asarray(self.w_[1:]), self.eta * X.T.dot( errors ) )
+
+            #   Calculate the gradient based on the whole training dataset
+            self.w_[0] += self.eta * errors.sum()
+
+            cost = (errors**2).sum() / 2.0
+            self.cost_.append(cost)
+
+        return self
+
+    def net_input(self, X):
+        """
+        Net input calculation for a given P.E.
+        Args:
+            X (numpy.ndarray): Feature matrix.
+
+        Returns:
+            numpy.ndarray Sum of net inputs
+        """
+
+        return np.dot( X, self.w_[1:] ) + self.w_[0]
+
+    def activation(self, X):
+        """
+        Method for computing the linear activation function.
+        Args:
+            X (numpy.ndarray): Feature matrix.
+
+        Returns:
+            numpy.ndarray Activation as calculated by the net input.
+        """
+
+        return self.net_input(X)
+
+    def predict(self, X):
+        """
+        Estimate the class label for a given pattern
+        Args:
+            X (numpy.ndarray): Feature matrix.
+
+        Returns:
+            ndarray: A Numpy array value with the expected (predicted) label of the pattern.
+        """
+
+        return np.where(self.activation(X) >= 0.0, 1, -1)
---
+++ single-layer-networks/perceptron/perceptron.py
@@ -0,0 +1,124 @@
+#!/usr/local/bin/python3
+
+# ---------------------------------------------------------------------------------------------------------------------
+#
+#                                        Bioinformatics Research Group
+# 									        http://biorg.cis.fiu.edu/
+#                             			Florida International University
+#
+#   This software is a "Camilo Valdes Work" under the terms of the United States Copyright Act.
+#   Please cite the author(s) in any work or product based on this material.
+#
+#   OBJECTIVE:
+#   The purpose of this program is to implement the Perceptron classifier.
+#
+#   NOTES:
+#   Please see the dependencies section below for the required libraries (if any).
+#
+#   DEPENDENCIES:
+#
+#       • Numpy
+#
+#   The above libraries & modules are required. You can check the modules currently installed in your
+#   system by running: python -c "help('modules')"
+#
+#   USAGE:
+#   Run the program with the "--help" flag to see usage instructions.
+#
+#   AUTHOR:	Camilo Valdes (cvalde03@fiu.edu)
+#           Bioinformatics Research Group,
+#           School of Computing and Information Sciences,
+#           Florida International University (FIU)
+#
+#
+# ---------------------------------------------------------------------------------------------------------------------
+
+# 	Python Modules
+import time
+import numpy as np
+
+class Perceptron(object):
+    """
+    Perceptron Classifier.
+    This module implements the Perceptron Learning algorithm.
+
+    Parameters:
+        eta (float): Learning rate (between 0.0 and 1.0)
+        n_iter (int): Number of passes over the entire dataset (epochs).
+
+    Attributes:
+        w_ (1-d array): Weights after fitting.
+        errors_ (list): Number of misclassifications in every epoch.
+    """
+
+    def __init__(self, eta=0.01, n_iter=10):
+        self.eta = eta
+        self.n_iter = n_iter
+
+    def fit(self, X, y):
+        """
+        Fits the training data, and allows the Perceptron algorithm to learn.
+        Args:
+            X (array-like): Training vectors, where n_samples is the number of samples and n_features is the number of
+            features. Shape = [n_samples, n_feature].
+            y (array-like): Target Values. Shape =[n_samples]
+
+        Returns:
+            self (object): Returns itself with updated weights.
+        """
+
+        #   Initialize weights to zero. Note shape[1] is number of columns, and shape[0] is number of rows.
+        self.w_ = np.zeros(1 + X.shape[1])
+
+        #   Track the misclassifications for a given epoch
+        self.errors_ = []
+
+        for _ in range(self.n_iter):
+
+            errors = 0
+
+            #   The 'zip()' function returns a list of tuples, where the i-th tuple contains the i-th element from
+            #   each of the argument sequences or iterables.
+            for xi, target in zip(X, y):
+
+                #   Perceptron Learning Rule. Recall that self.eta is the learning rate.
+                update = self.eta * ( target - self.predict( xi ) )
+
+                #   Update the weights (including the bias)
+                self.w_[1:] += update * xi
+                self.w_[0] += update
+
+                #   Stopping Condition - Keep track of the errors
+                errors += int(update != 0.0)
+
+            self.errors_.append(errors)
+
+        return self
+
+    def net_input(self, X):
+        """
+        Calculates the Net Input for a neuron.
+        Args:
+            X (array-like): Training vectors, where n_samples is the number of samples and n_features is the number of
+            features. Shape = [n_samples, n_feature].
+
+        Returns:
+            float: The net input (dot product) calculated from the input layer.
+        """
+
+        #   Return the dot-product of w (transposed) and x
+        #   Note: self.w_[0] is basically the "threshold" or so-called "bias unit."
+        # print("Bias: " + str(self.w_[0]))
+        return np.dot(X, self.w_[1:]) + self.w_[0]
+
+    def predict(self, X):
+        """
+        Returns the class label after a unit step.
+        Args:
+            X (array-like): Training vectors, where n_samples is the number of samples and n_features is the number of
+            features. Shape = [n_samples, n_feature].
+
+        Returns:
+            ndarray: A Numpy array value with the expected (predicted) label of the pattern.
+        """
+        return np.where(self.net_input(X) >= 0.0, 1, -1)
